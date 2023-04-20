d8de2185d4c2738f6083b134156a8ba9274f6bf8
Baheryan <baheryan.s@gmail.com>
2023-04-20 17:36:48 +0300

    import app-add-form component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import AppInfo from '../app-info/app-info';
 import SearchPanel from '../app-search-panel/app-search-panel';
 import AppFilter from '../app-filter/app-filter';
 import AppList from '../app-list/app-list';
+import AppAddForm from '../app-add-form/app-add-form';
 
 import './app.css';
 
@@ -14,6 +15,7 @@ function App() {
                 <AppFilter/>
             </div>
             <AppList/>
+            <AppAddForm/>
         </div>
     )
 }
